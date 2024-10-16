e8e5a21dcb121052dfb2e3c3a02eaee7d2ebbde1
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2024-10-16 16:44:24 -0300

    Add files via upload

---
+++ TesteO/src/main/java/com/mycompany/testeo/TesteO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testeo;
+
+/**
+ *
+ * @author wpass
+ */
+public class TesteO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
